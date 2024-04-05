7022c5c40d48adae94e73cff0ee635113eef7189
Mathieu <bon.mathieu@gmail.com>
2024-04-05 13:50:10 +0200

    Wrote all functions and tests.

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Constants {
+
+    public static final int UPDATE_PATCH_VALUE = 1;
+    public static final int UPDATE_MINEUR_VALUE = 10;
+    public static final int UPDATE_MAJEUR_VALUE = 100;
+}
